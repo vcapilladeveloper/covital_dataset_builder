9604516c8e945a19bf28169d8f5dc169f862665f
Victor Capilla <victorcapilladeveloper@gmail.com>
2020-04-09 22:07:23 +0200

    Change text error for unhandle camera
    Change text error for unhandle camera

--- lib/home.dart
+++ lib/home.dart
@@ -273,7 +273,7 @@ class _HomeState extends State<Home> {
 
   void _showCameraException(CameraException e) {
     print(e.code + " " + e.description);
-    showInSnackBar('Error: ${e.code}\n${e.description}');
+    showInSnackBar('${e.description}\nCamera is disabled inside App settings');
   }
 
   void showInSnackBar(String message) {
